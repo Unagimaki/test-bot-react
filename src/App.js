dc725f8849279d3346299fd5aa28f0fc948076a7
Mikhail <mikhailyarkov2000@gmail.com>
2024-06-24 23:00:20 +0400

    added data from bot

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ function App() {
   useEffect(() => {
     tg.ready()
   }, [tg])
+  useEffect(() => {
+    const startParams = new URLSearchParams(window.location.search);
+    const foo = startParams.get('foo');
+    const baz = startParams.get('baz');
+    console.log(foo, baz); // output: bar qux
+    // use the data as needed in your React app
+  }, []);
 
   return (
     <div className="App">
