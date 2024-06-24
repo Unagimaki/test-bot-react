0bd3e3726528c0fde0934e3482e477783d544315
Mikhail <mikhailyarkov2000@gmail.com>
2024-06-25 00:27:30 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -10,9 +10,15 @@ function App() {
   const {tg, onToggleButton, chat} = useTelegram()
 
   useEffect(() => {
-    tg.ready()
-    alert('chat: ' + chat)
-  }, [tg, chat])
+    const initData = window.Telegram.WebApp.initData;
+    if (initData) {
+      const startParams = JSON.parse(initData);
+      const foo = startParams.foo;
+      const baz = startParams.baz;
+      alert(foo, baz); // output: bar qux
+      // use the data as needed in your React app
+    }
+  }, [initData]);
 
 
   return (
